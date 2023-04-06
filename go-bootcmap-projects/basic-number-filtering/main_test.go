37a8537f34dd073997c2bc5d5c9a349146b323af
pravytecy <pravyedtech@gmail.com>
2023-04-06 21:51:38 +0530

    added test case for filter

--- go-bootcmap-projects/basic-number-filtering/main_test.go
+++ go-bootcmap-projects/basic-number-filtering/main_test.go
@@ -42,5 +42,46 @@ func TestFilter(t *testing.T) {
 	if !reflect.DeepEqual(output, want) {
 		t.Errorf("wanted: %v but got: %v", want, output)
 	}
+}
 
+func TestWithMultipleCondtions(t *testing.T) {
+	odd := func(n int) bool { return n%2 != 0 }
+	even := func(n int) bool { return !odd(n) }
+	greaterThan := func(n int) Condition {
+		return func(m int) bool {
+			return m > n
+		}
+	}
+
+	greaterThan5 := greaterThan(5)
+	multiplesOf := func(n int) Condition { return func(m int) bool { return m%n == 0 } }
+	multiplesOf3 := multiplesOf(3)
+
+	lessThanN := func(n int) Condition { return func(m int) bool { return m < n } }
+	lessThan15 := lessThanN(15)
+
+	tt := []struct {
+		nums  []int
+		conds []Condition
+		want  []int
+	}{
+
+		{
+			nums:  []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
+			conds: []Condition{odd, greaterThan5, multiplesOf3},
+			want:  []int{9, 15},
+		},
+		{
+			nums:  []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
+			conds: []Condition{even, lessThan15, multiplesOf3},
+			want:  []int{6, 12},
+		},
+	}
+
+	for _, opt := range tt {
+		output := filter(opt.nums, opt.conds...)
+		if !reflect.DeepEqual(output, opt.want) {
+			t.Errorf("wanted: %v but got: %v", opt.want, output)
+		}
+	}
 }
